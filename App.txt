c20bae76058452fd45e9cbcd9539cb9ce01dfbed
G Devendrababu <116060950+G-DevendraBabu@users.noreply.github.com>
2023-11-03 09:04:03 +0530

    change-1

---
+++ App.txt
@@ -0,0 +1,16 @@
+// import DailyTimeSpentChart from './DailyTimeSpentChart';
+import { Assignment } from './Assignment';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+     
+      <Assignment/>
+      
+     </div>
+    
+  );
+}
+
+export default App;
---
+++ Assignment.txt.js
@@ -0,0 +1,305 @@
+import React from "react";
+import "./App.css";
+
+export const Assignment = () => {
+    return (
+        <div className="assignment">
+            <div className="div">
+                <div className="overlap">
+                    <div className="group">
+                        <p className="about-flow-zone">
+                            <span className="text-wrapper">About </span>
+                            <span className="span">Flow Zone</span>
+                        </p>
+                    </div>
+                    <img className="img" alt="Group" src="group-3.png" />
+                </div>
+                <div className="overlap-group">
+                    <div className="div-wrapper">
+                        <div className="text-wrapper-2">Flow Peaks</div>
+                    </div>
+                    <div className="frame">
+                        <div className="text-wrapper-3">Weekly</div>
+                    </div>
+                    <div className="overlap-2">
+                        <div className="text-wrapper-4">My Flow Peaks</div>
+                        <div className="text-wrapper-5">02PM - 08PM</div>
+                        <img className="img-2" alt="Clockicon" src="clockicon.png" />
+                    </div>
+                    <div className="overlap-3">
+                        <div className="text-wrapper-4">Global Flow Peaks</div>
+                        <div className="text-wrapper-5">04PM - 11PM</div>
+                        <img className="img-2" alt="Time" src="time-1.png" />
+                    </div>
+                    <div className="overlap-4">
+                        <div className="overlap-group-wrapper">
+                            <div className="ellipse-wrapper">
+                                <img className="ellipse" alt="Ellipse" src="ellipse-470.svg" />
+                            </div>
+                        </div>
+                        <div className="overlap-wrapper">
+                            <div className="overlap-5">
+                                <img className="line" alt="Line" src="line-155.svg" />
+                                <img className="line-2" alt="Line" src="line-156.svg" />
+                            </div>
+                        </div>
+                        <div className="group-2">
+                            <div className="line-wrapper">
+                                <img className="line-3" alt="Line" src="line-44-2.svg" />
+                            </div>
+                            <div className="img-wrapper">
+                                <img className="line-3" alt="Line" src="line-47-2.svg" />
+                            </div>
+                            <div className="overlap-6">
+                                <img className="line-3" alt="Line" src="line-45-2.svg" />
+                            </div>
+                            <div className="overlap-7">
+                                <img className="line-3" alt="Line" src="line-48-3.svg" />
+                            </div>
+                            <div className="overlap-8">
+                                <img className="line-3" alt="Line" src="line-46-3.svg" />
+                            </div>
+                            <div className="overlap-9">
+                                <img className="line-3" alt="Line" src="line-49-3.svg" />
+                            </div>
+                            <div className="overlap-10">
+                                <img className="line-4" alt="Line" src="line-44-3.svg" />
+                            </div>
+                            <div className="overlap-11">
+                                <img className="line-4" alt="Line" src="line-47-3.svg" />
+                            </div>
+                            <div className="overlap-12">
+                                <img className="line-4" alt="Line" src="line-45-3.svg" />
+                            </div>
+                            <div className="overlap-13">
+                                <img className="line-4" alt="Line" src="line-48-4.svg" />
+                            </div>
+                            <div className="overlap-14">
+                                <img className="line-4" alt="Line" src="line-46-4.svg" />
+                            </div>
+                            <div className="overlap-15">
+                                <img className="line-4" alt="Line" src="line-49-4.svg" />
+                            </div>
+                            <div className="overlap-16">
+                                <img className="line-4" alt="Line" src="line-44-4.svg" />
+                            </div>
+                            <div className="overlap-17">
+                                <img className="line-4" alt="Line" src="line-50-2.svg" />
+                            </div>
+                            <div className="overlap-18">
+                                <img className="line-3" alt="Line" src="line-53.svg" />
+                            </div>
+                            <div className="overlap-19">
+                                <img className="line-3" alt="Line" src="line-56.svg" />
+                            </div>
+                            <div className="overlap-20">
+                                <img className="line-4" alt="Line" src="line-47-4.svg" />
+                            </div>
+                            <div className="overlap-21">
+                                <img className="line-4" alt="Line" src="line-45-4.svg" />
+                            </div>
+                            <div className="overlap-22">
+                                <img className="line-3" alt="Line" src="line-51-3.svg" />
+                            </div>
+                            <div className="overlap-23">
+                                <img className="line-3" alt="Line" src="line-54-2.svg" />
+                            </div>
+                            <div className="overlap-24">
+                                <img className="line-4" alt="Line" src="line-57-2.svg" />
+                            </div>
+                            <div className="overlap-25">
+                                <img className="line-4" alt="Line" src="line-48-5.svg" />
+                            </div>
+                            <div className="overlap-26">
+                                <img className="line-4" alt="Line" src="line-46-5.svg" />
+                            </div>
+                            <div className="overlap-27">
+                                <img className="line-3" alt="Line" src="line-52-3.svg" />
+                            </div>
+                            <div className="overlap-28">
+                                <img className="line-4" alt="Line" src="line-55-2.svg" />
+                            </div>
+                            <div className="overlap-29">
+                                <img className="line-4" alt="Line" src="line-58-2.svg" />
+                            </div>
+                            <div className="overlap-30">
+                                <img className="line-4" alt="Line" src="line-49-5.svg" />
+                            </div>
+                            <div className="overlap-31">
+                                <img className="line-3" alt="Line" src="line-44-5.svg" />
+                            </div>
+                            <div className="overlap-32">
+                                <img className="line-3" alt="Line" src="line-47-5.svg" />
+                            </div>
+                            <div className="overlap-33">
+                                <img className="line-3" alt="Line" src="line-45-5.svg" />
+                            </div>
+                            <div className="overlap-34">
+                                <img className="line-3" alt="Line" src="line-48-6.svg" />
+                            </div>
+                            <div className="overlap-35">
+                                <img className="line-3" alt="Line" src="line-46-6.svg" />
+                            </div>
+                            <div className="overlap-36">
+                                <img className="line-3" alt="Line" src="line-49-6.svg" />
+                            </div>
+                            <div className="overlap-37">
+                                <img className="line-3" alt="Line" src="line-50-3.svg" />
+                            </div>
+                            <div className="overlap-38">
+                                <img className="line-4" alt="Line" src="line-51-4.svg" />
+                            </div>
+                            <div className="overlap-39">
+                                <img className="line-3" alt="Line" src="line-52-4.svg" />
+                            </div>
+                            <div className="overlap-40">
+                                <img className="line-5" alt="Line" src="line-38-3.svg" />
+                            </div>
+                            <div className="overlap-41">
+                                <img className="line-6" alt="Line" src="line-40-3.svg" />
+                            </div>
+                            <div className="overlap-42">
+                                <div className="text-wrapper-6">2</div>
+                                <div className="text-wrapper-6">2</div>
+                            </div>
+                            <div className="overlap-43">
+                                <div className="text-wrapper-6">8</div>
+                                <div className="text-wrapper-6">8</div>
+                            </div>
+                            <div className="overlap-44">
+                                <div className="text-wrapper-7">10</div>
+                                <div className="text-wrapper-7">10</div>
+                            </div>
+                            <div className="overlap-45">
+                                <img className="line-7" alt="Line" src="line-27-2.svg" />
+                            </div>
+                            <div className="overlap-46">
+                                <img className="line-8" alt="Line" src="line-35-2.svg" />
+                            </div>
+                            <div className="overlap-47">
+                                <img className="line-7" alt="Line" src="line-28-2.svg" />
+                            </div>
+                            <div className="overlap-48">
+                                <img className="line-5" alt="Line" src="line-37-3.svg" />
+                            </div>
+                            <div className="overlap-49">
+                                <img className="line-6" alt="Line" src="line-39-3.svg" />
+                            </div>
+                            <div className="overlap-50">
+                                <img className="line-5" alt="Line" src="line-37-4.svg" />
+                            </div>
+                            <div className="overlap-51">
+                                <img className="line-6" alt="Line" src="line-39-4.svg" />
+                            </div>
+                            <div className="overlap-52">
+                                <img className="line-5" alt="Line" src="line-38-4.svg" />
+                            </div>
+                            <div className="overlap-53">
+                                <img className="line-6" alt="Line" src="line-40-4.svg" />
+                            </div>
+                            <div className="overlap-54">
+                                <img className="line-8" alt="Line" src="line-36-2.svg" />
+                            </div>
+                            <div className="overlap-55">
+                                <div className="text-wrapper-8">0</div>
+                                <div className="text-wrapper-8">0</div>
+                            </div>
+                            <div className="overlap-56">
+                                <div className="text-wrapper-9">18</div>
+                                <div className="text-wrapper-9">18</div>
+                            </div>
+                            <div className="overlap-57">
+                                <div className="text-wrapper-10">12</div>
+                                <div className="text-wrapper-10">12</div>
+                            </div>
+                            <div className="overlap-58">
+                                <div className="text-wrapper-8">6</div>
+                                <div className="text-wrapper-8">6</div>
+                            </div>
+                            <div className="overlap-59">
+                                <div className="text-wrapper-6">4</div>
+                                <div className="text-wrapper-6">4</div>
+                            </div>
+                            <div className="overlap-60">
+                                <div className="text-wrapper-7">14</div>
+                                <div className="text-wrapper-7">14</div>
+                            </div>
+                            <div className="overlap-61">
+                                <div className="text-wrapper-7">16</div>
+                                <div className="text-wrapper-7">16</div>
+                            </div>
+                            <div className="overlap-62">
+                                <div className="text-wrapper-11">20</div>
+                                <div className="text-wrapper-11">20</div>
+                            </div>
+                            <div className="overlap-63">
+                                <div className="text-wrapper-7">22</div>
+                                <div className="text-wrapper-7">22</div>
+                            </div>
+                        </div>
+                        <div className="text-wrapper-12">Night</div>
+                        <div className="text-wrapper-13">Morning</div>
+                        <div className="text-wrapper-14">Afternoon</div>
+                        <div className="text-wrapper-15">Evening</div>
+                    </div>
+                    <img className="group-3" alt="Group" src="group-4.png" />
+                </div>
+                <div className="group-4">
+                    <div className="text-wrapper-2">My Thriving Toolkit</div>
+                </div>
+                <div className="overlap-64">
+                    <div className="overlap-65">
+                        <div className="rectangle" />
+                        <div className="rectangle-2" />
+                        <div className="rectangle-3" />
+                        <div className="rectangle-4" />
+                        <div className="overlap-66">
+                            <div className="rectangle-5" />
+                            <div className="rectangle-6" />
+                            <div className="text-wrapper-16">Affirmation</div>
+                            <div className="text-wrapper-17">Thought Guides</div>
+                        </div>
+                        <div className="text-wrapper-18">For You</div>
+                        <img className="globallyicon" alt="Globallyicon" src="globallyicon.png" />
+                        <div className="text-wrapper-19">Globally</div>
+                        <img className="foryouicon" alt="Foryouicon" src="foryouicon.png" />
+                        <div className="frame-2">
+                            <div className="text-wrapper-20">Explore</div>
+                        </div>
+                        <div className="frame-3">
+                            <div className="text-wrapper-21">Memory Game</div>
+                        </div>
+                        <div className="text-wrapper-22">Lorem Ipsum</div>
+                        <div className="text-wrapper-23">Lorem Ipsum</div>
+                        <div className="text-wrapper-24">Lorem Ipsum</div>
+                        <div className="text-wrapper-25">Lorem Ipsum</div>
+                    </div>
+                    <img className="group-5" alt="Group" src="group-647.svg" />
+                    <div className="text-wrapper-26">Games</div>
+                    <div className="text-wrapper-27">Tools</div>
+                    <div className="text-wrapper-28">Lessons</div>
+                    <div className="text-wrapper-29">Clubs</div>
+                    <img className="tools-icon" alt="Tools icon" src="tools-icon.png" />
+                    <img className="lessonsicon" alt="Lessonsicon" src="lessonsicon.png" />
+                    <img className="clubicon" alt="Clubicon" src="clubicon.png" />
+                </div>
+                <div className="rectangle-7" />
+                <div className="group-6">
+                    <div className="overlap-67">
+                        <div className="group-7">
+                            <div className="text-wrapper-30">9:25</div>
+                            <img className="group-8" alt="Group" src="group.png" />
+                            <img className="group-9" alt="Group" src="image.png" />
+                            <img className="group-10" alt="Group" src="group-2.png" />
+                        </div>
+                        <div className="text-wrapper-31">Flow Zone</div>
+                        <div className="vector-wrapper">
+                            <img className="vector" alt="Vector" src="vector.svg" />
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
